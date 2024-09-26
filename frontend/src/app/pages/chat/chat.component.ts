173e73928c70dc7a1fcdddbae154348f37f3de0e
William Bechard <william.j.bechard@gmail.com>
2024-09-26 17:33:27 -0500

    chat screen init setup

--- frontend/src/app/pages/chat/chat.component.ts
+++ frontend/src/app/pages/chat/chat.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageModule } from "primeng/image";
+import { IonicModule } from "@ionic/angular";
+import { FormsModule } from "@angular/forms";
+import { ButtonModule } from "primeng/button";
+import { AstroComponentsModule } from "@astrouxds/angular";
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
+  imports: [
+    CommonModule,
+    ImageModule,
+    IonicModule,
+    FormsModule,
+    ButtonModule,
+    AstroComponentsModule,
+  ],
+  standalone: true
 })
 export class ChatComponent  implements OnInit {
 
